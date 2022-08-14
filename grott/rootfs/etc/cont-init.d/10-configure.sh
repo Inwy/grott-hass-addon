1ebd338682e3bc2ba7e4099c1e8114566e9beba1
Edwin Bosveld <edwin@inwy.nl>
2022-08-14 21:44:45 +0200

    Improve configuration logging

--- grott/rootfs/etc/cont-init.d/10-configure.sh
+++ grott/rootfs/etc/cont-init.d/10-configure.sh
@@ -4,11 +4,17 @@
 # Configures Grott
 # ==============================================================================
 
+bashio::log "Configuring Grott using the add-on configuration"
+
 if  bashio::config.has_value 'verbose'; then export gverbose="$(bashio::config 'gverbose')"; fi
 
 if ! bashio::services.available "mqtt"; then
     bashio::exit.nok "No internal MQTT service available"
 else
+    bashio::log "Gathering MQTT information from internal services"
+
+    bashio::log "$(bashio::services "mqtt" "host")"
+
     export gmqttip="$(bashio::services "mqtt" "host")"
     export gmqttport="$(bashio::services "mqtt" "port")"
     export gmqttauth="True"
