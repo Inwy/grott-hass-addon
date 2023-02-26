bc039f6b0f0aa63f96dd56f1d2eaae0480a521be
Edwin Bosveld <edwin@inwy.nl>
2023-02-26 13:38:47 +0100

    feat: better configuration for mqtt

--- grott/rootfs/etc/cont-init.d/10-configure.sh
+++ grott/rootfs/etc/cont-init.d/10-configure.sh
@@ -19,26 +19,38 @@ if  bashio::config.has_value 'verbose'; then
     crudini --set "$CONFIG" Generic verbose "$(bashio::config 'verbose')" 
 fi
 
-if  bashio::config.has_value 'mqtt_ip'; then 
-    bashio::log "Setting MQTT IP"
+if  bashio::services.available 'mqtt'; then 
+    bashio::log "Setting MQTT settings"
     crudini --set "$CONFIG" MQTT nomqtt "False" 
-    crudini --set "$CONFIG" MQTT ip "$(bashio::config 'mqtt_ip')" 
-fi
-
-if  bashio::config.has_value 'mqtt_port'; then 
-    bashio::log "Setting MQTT port"
-    crudini --set "$CONFIG" MQTT port "$(bashio::config 'mqtt_port')" 
-fi
-
-if  bashio::config.has_value 'mqtt_user'; then 
-    bashio::log "Setting MQTT user"
+    crudini --set "$CONFIG" MQTT ip "$(bashio::services mqtt "host")" 
+    crudini --set "$CONFIG" MQTT port "$(bashio::services mqtt "port")" 
     crudini --set "$CONFIG" MQTT auth "True" 
-    crudini --set "$CONFIG" MQTT user "$(bashio::config 'mqtt_user')" 
+    crudini --set "$CONFIG" MQTT user "$(bashio::services mqtt "username")" 
+    crudini --set "$CONFIG" MQTT password "$(bashio::services mqtt "password")" 
+else
+    bashio::exit.nok "MQTT service is not available"
 fi
 
-if  bashio::config.has_value 'mqtt_password'; then 
-    bashio::log "Setting MQTT password"
-    crudini --set "$CONFIG" MQTT password "$(bashio::config 'mqtt_password')" 
+if  bashio::config.has_value 'pvoutput'; then 
+    bashio::log "Setting PVOutput settings"
+
+    pvOutputSettings = $(bashio::config 'pvoutput')
+
+    crudini --set "$CONFIG" PVOutput pvoutput "True" 
+    crudini --set "$CONFIG" PVOutput apikey "$(pvOutputSettings.apikey)"
+
+    inverters = pvOutputSettings.inverters
+
+    if [${#inverters[@]} -gt 1]; then
+        crudini --set "$CONFIG" PVOutput pvinterters "$(invertersLenght)"
+        for i in pvOutputSettings.inverters:
+            inverter = pvOutputSettings.inverters[i]
+
+            crudini --set "$CONFIG" PVOutput "systemid".format "$(pvOutputSettings.inverters[].systemId)"
+            crudini --set "$CONFIG" PVOutput systemid "$(pvOutputSettings.inverters[0].systemId)"
+    else
+        crudini --set "$CONFIG" PVOutput systemid "$(pvOutputSettings.inverters[0].systemId)"
+    fi
 fi
 
 bashio::log "Done changing configuration"
